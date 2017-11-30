04f29f43fb8d653b34e5e1ed0681ba2fc8de5af5
Christian Kühnel <christian.kuehnel@gmail.com>
2017-11-30 15:31:33 +0100

    added setup method for tests

---
+++ test/test_btrfs.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import unittest
+from datetime import datetime
+from subprocess import check_call
+
+
+class TestBtrfs(unittest.TestCase):
+
+    ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    TEMP_DIR = os.path.join(ROOT_DIR, 'tmp')
+    MOUNT_POINT = os.path.join(TEMP_DIR, 'mnt')
+    IMG_FILE = os.path.join(TEMP_DIR, 'btrfs.img')
+    TEST_FILE = os.path.join(MOUNT_POINT, 'test_file')
+
+    @classmethod
+    def setUpClass(cls):
+        if os.path.ismount(cls.MOUNT_POINT):
+            check_call('umount {}'.format(cls.MOUNT_POINT), shell=True)
+        if os.path.exists(cls.TEMP_DIR):
+            shutil.rmtree(cls.TEMP_DIR)
+        os.makedirs(os.path.join(cls.TEMP_DIR, cls.MOUNT_POINT))
+        check_call('dd if=/dev/zero of={} bs=1M count=100'.format(cls.IMG_FILE), shell=True)
+        check_call('mkfs.btrfs {}'.format(cls.IMG_FILE), shell=True)
+        check_call('mount -o loop {} {}'.format(cls.IMG_FILE, cls.MOUNT_POINT), shell=True)
+        with open(cls.TEST_FILE, 'w') as test_file:
+            test_file.write(datetime.now().isoformat()+"\n")
+
+    def test_nothing(self):
+        pass
